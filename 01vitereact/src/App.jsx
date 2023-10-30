9741cc90495a987109ad62fc0cc5d65694c89180
Abrar A <kh.abrar22@gmail.com>
2023-10-31 02:50:41 +0500

    Custom React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -5,10 +5,13 @@ import Tea from './Tea.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
+  const userName = "Abrar!"
+
   return (
     <>
     <Tea />
-    <h1>React Vite | Abrar</h1>
+    {/* Evaluated Expression */}
+    <h1>React Vite | {userName}</h1>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,38 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1>Cunstom App</h1>
+//     </div>
+//   )
+// }
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit Google'
+// }
+
+// const anotherElement = (
+//   <a href='https://google.com'>Direct Element</a>
+// )
+
+// const anotherUser = ". Arqam"
+
+// === React Special Syntax
+// const ReactElement = React.createElement(
+//   'a',
+//   {href: 'https://google.com', target: '_blank'},
+//   'Visit Google via react Element',
+//   anotherUser
+// )
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
+  // ReactElement
 )
